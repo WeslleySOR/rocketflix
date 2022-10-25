9c83782caaf74d5f2358eb1357ac32cfc6b0e1a0
WeslleySOR <weslleywruas@gmail.com>
2022-10-25 18:48:29 -0300

    feat: base layout finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,38 @@
+// import { useEffect } from "react"
+
 export function Home() {
+  // useEffect(() => {
+  //   console.log(import.meta.env.VITE_API_KEY)
+  // }, [])
   return (
-    <main>
-      Home Page
+    <main className="h-screen flex flex-col items-center gap-8 py-32">
+      <div className="flex flex-col w-fit items-center gap-3">
+        <div className="w-20 h-14">
+          <img
+            className="h-full w-full"
+            src="/assets/shuffle.svg"
+            alt="Icone de sortear"
+          />
+        </div>
+        <h1 className="text-[#FFFCF9] text-4xl font-bold">
+          Não sabe o que assistir?
+        </h1>
+      </div>
+      {/* TODO: Aqui irá o component do filme escolhido */}
+      <button className="flex gap-4 justify-center items-center bg-[#E9E6E3] p-4 rounded-md">
+        <div className="w-9 h-6">
+          <img
+            className="h-full w-full"
+            src="/assets/shuffle.svg"
+            alt="Icone de sortear"
+          />
+        </div>
+        <span className="text-[#000] text-sm font-bold">Encontrar filme</span>
+      </button>
+      <span className="text-[#FFFCF9] text-base font-normal">
+        Clique em "Encontrar filme" que traremos informações de algum filme para
+        você assistir hoje.
+      </span>
     </main>
-  )
+  );
 }
